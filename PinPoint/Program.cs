4b74f5df123322d4b07cfde838fd0cd1b1516edb
Masato.H <30688976+matto1357@users.noreply.github.com>
2022-01-29 20:56:33 +0900

    Add csharp projectfiles

---
+++ PinPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PinPoint
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("MynameisHirokawa");
+        }
+    }
+}
---
+++ PinPoint/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Math = System.Math;
+
+namespace PinPoint
+{
+	//Util Class
+    public static class Utils
+    {
+		//MathUtil
+		public static List<int> GenerateNums(int min, int max, int count, bool isDuplicate){
+			List<int> result = new List<int>();
+			
+			return result;
+		}
+    }
+}
